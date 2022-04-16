0d768eceab374e3ba846167cc3ab8f6625f2ddfe
AgienkoWebDev <AgienkoWebDev@gmail.com>
2022-04-16 16:59:14 +0300

    adding logo and fonts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './fonts/raleway.css'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     fghjfgkhfkfkvkvgk дгшрдглрдшгнрдшг
+    </div>
+  );
+}
+
+export default App;
